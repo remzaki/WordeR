0d1e33c35a2584cd0c54007d9d7abf7d821dbce4
remzaki <reimark.cogonon@gmail.com>
2017-12-16 15:12:20 +0800

    Add options for arguments

--- WordeR.py
+++ WordeR.py
@@ -1,22 +1,23 @@
 import itertools
 import random
-
-dictionary = [i.strip('\n') for i in open('dictionary.txt')]
-s = "RBNIAY"
+from sys import argv
 
 
-def get_words(new_s, word_list):
+def get_words(new_s, word_list, n=3):
     if not new_s:
         return word_list
     else:
         possibilities = [i for i in dictionary if all(new_s.count(b) >= i.count(b) for b in i)]
         y = 0
         x = len(possibilities)
+        f = open('.\%s.txt' % new_s, 'w+')
         while x > y:
             w = possibilities[y]
-            if len(w) >= 3:
+            if len(w) >= n:
                 print w
+                f.write(str(y) + '. ' + w + '\n')
             y += 1
+        f.close()
         if not possibilities:
             return get_words([], word_list)
         else:
@@ -28,5 +29,14 @@ def get_words(new_s, word_list):
             return get_words(new_final_word, word_list)
 
 
-final_words = get_words(s, [])
-print(final_words)
+# final_words = get_words(s, [])
+# print(final_words)
+
+
+if __name__ == '__main__':
+    dictionary = [i.strip('\n') for i in open('dictionary.txt')]
+    # s = "EMASSE"
+    s = (argv[1]).upper()
+    n = int(argv[2])
+    # print n
+    final_words = get_words(s, [], n)
